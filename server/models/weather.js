3292235128f73182f4cbe820c599e3820960d9c5
Durian <karen.kua7@gmail.com>
2019-03-20 00:06:02 -0400

    Fixed errors with models

---
+++ server/models/weather.js
@@ -0,0 +1,19 @@
+const request = require("request-promise")
+
+const API_KEY = "be74cd73586e8b47e96bff06290076ce" 
+
+class Weather {
+    static retrieveByCity(city, callback) {
+        request({
+            uri: `https://api.openweathermap.org/data/2.5/weather?q=${city}&APPID=${API_KEY}&units=imperial`,
+            json: true
+        })
+        .then((res) => callback(res))
+        .catch((err) => {
+            console.log(err)
+            callback({error: "Could not reach OpenWatherMap API."})
+        })
+    }
+}
+
+module.exports = Weather
